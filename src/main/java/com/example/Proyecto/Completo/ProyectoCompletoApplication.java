9b986f4c70aa5015da04bd18622c7351db610125
Emilio <emilioaperezvilla@ourlook.com>
2023-02-06 22:09:22 -0600

    Cors updt

--- src/main/java/com/example/Proyecto/Completo/ProyectoCompletoApplication.java
+++ src/main/java/com/example/Proyecto/Completo/ProyectoCompletoApplication.java
@@ -2,11 +2,23 @@ package com.example.Proyecto.Completo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProyectoCompletoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoCompletoApplication.class, args);
 	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+
 }
